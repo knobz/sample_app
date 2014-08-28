9ad43d046feed6e0ef467279843c3ac243aec119
Benjamin Stein <ben.e.stein@gmail.com>
2014-08-28 03:19:07 -0700

    Added contact to static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,17 @@
 class StaticPagesController < ApplicationController
+#Home page
   def home
   end
 
+#Help page
   def help
   end
-
+#About page
   def about
   end
-  
+
+#Contact
+  def contact
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,12 +35,27 @@ describe "Static pages" do
 	describe "About page" do
 		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
+			page.should have_selector('h1', 
+				:text => 'About Us')
 		end
 			it "should have the title 'About Us'" do
 			visit '/static_pages/about' 
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+
+	## Contact page
+	describe "Contact page" do
+		it "should have h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', 
+				:text => "Contact")
+		end
+		it "should have title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				:text => "Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
