8fe96ac79bee892113b12e8ef591ff1db786d92c
Benjamin Stein <ben.e.stein@gmail.com>
2014-08-28 03:29:18 -0700

    Updated gitignore

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
-
 describe "Static pages" do
+
+	## Define glabal site title
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
    ## Home Page
    describe "Home page" do
     it "should have the h1 'Sample App'" do
@@ -13,7 +16,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-					:text =>"Ruby on Rails Tutorial Sample App | Home")
+					:text => "#{base_title} | Home")
 	end
   end
 
@@ -27,7 +30,7 @@ describe "Static pages" do
 		it "should have the title 'Help'" do 
 			visit '/static_pages/help' 
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Help")
+				:text => "#{base_title} | Help")
 		end
 	end
 
@@ -41,7 +44,7 @@ describe "Static pages" do
 			it "should have the title 'About Us'" do
 			visit '/static_pages/about' 
 			page.should have_selector('title',
-				:text => "Ruby on Rails Tutorial Sample App | About Us")
+				:text => "#{base_title} | About Us")
 		end
 	end
 
@@ -55,7 +58,7 @@ describe "Static pages" do
 		it "should have title 'Contact'" do
 			visit '/static_pages/contact'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Contact")
+				:text => "#{base_title} | Contact")
 		end
 	end
 end
